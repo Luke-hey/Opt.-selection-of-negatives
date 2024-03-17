b418346ab5883f43e840c6001bb5159e7bd8c1c9
Luke-hey <57324207+Luke-hey@users.noreply.github.com>
2024-03-17 23:42:10 +0100

    Update main_script.py

--- scripts/main_script.py
+++ scripts/main_script.py
@@ -2,31 +2,37 @@ import subprocess
 import argparse
 import os
 
+def get_script_path(script_name):
+    # Get the directory of the currently executing script
+    current_dir = os.path.dirname(os.path.realpath(__file__))
+    # Construct the path to the script
+    return os.path.join(current_dir, script_name)
+
 def run_pad_positives(input_pos, flank_size):
     # Run pad_positives.py with the specified input and flank size
-    command = ['python', 'pad_positives.py', input_pos, '--flank_size', str(flank_size)]
+    command = ['python', get_script_path('pad_positives.py'), input_pos, '--flank_size', str(flank_size)]
     subprocess.run(command)
 
 def run_bed_to_fasta(input_pos):
     # Run bed_to_fasta.py with the specified input
     output_fasta = os.path.splitext(os.path.basename(input_pos))[0] + "_adjusted.fa"
-    command = ['python', 'bed_to_fasta.py', input_pos, '--output_file', output_fasta]
+    command = ['python', get_script_path('bed_to_fasta.py'), input_pos, '--output_file', output_fasta]
     subprocess.run(command)
 
 def run_bedtools_script(input_pos, input_low_conf1, input_low_conf2, output_prefix):
     # Run bedtools_script.py with the specified inputs and output prefix
-    command = ['python', 'bedtools_script.py', input_pos, input_low_conf1, input_low_conf2, output_prefix]
+    command = ['python', get_script_path('bedtools_script.py'), input_pos, input_low_conf1, input_low_conf2, output_prefix]
     subprocess.run(command)
 
-def run_sampling_script(genes_without_pos_file, genes_without_pos, positives_file, num_samples, output_prefix, sampling_mode):
+def run_sampling_script(genes_without_pos_file, genes_without_pos, positives_file, positives_bed, input_low_conf1, input_low_conf2, num_samples, output_prefix, sampling_mode):
     # Run the appropriate sampling script based on the sampling mode
     if sampling_mode == 'hard':
         output_file = f"{output_prefix}_hard_negatives_output{num_samples}.fa"
-        command = ['python', 'sampling_hardneg.py', genes_without_pos, positives_file, output_file, '--num_sequences_to_save', str(num_samples)]
+        command = ['python', get_script_path('sampling_hardneg.py'), genes_without_pos, positives_file, output_file, '--num_sequences_to_save', str(num_samples)]
         subprocess.run(command)
     else:
         output_file = f"{output_prefix}_normal_negatives_output{num_samples}.bed"
-        command = ['python', 'generate_negatives.py', genes_without_pos_file, output_file, '--num_samples', str(num_samples)]
+        command = ['python', get_script_path('generate_negatives.py'), genes_without_pos_file, positives_bed, input_low_conf1, input_low_conf2, output_file, '--num_samples', str(num_samples)]
         subprocess.run(command)
 
 def main():
@@ -49,8 +55,9 @@ def main():
     # Step 3: Run bedtools_script.py
     run_bedtools_script(args.input_pos, args.input_low_conf1, args.input_low_conf2, args.output_prefix)
     run_bed_to_fasta(f"{args.output_prefix}_genes_without_pos.bed")
+
     # Step 4: Run sampling script (hard or normal)
-    run_sampling_script(genes_without_pos_file=f"{args.output_prefix}_genes_without_pos.bed", genes_without_pos=f"{args.output_prefix}_genes_without_pos.fa", positives_file = f"{args.input_pos.split('.')[0]}.fa", num_samples=args.num_samples, output_prefix=args.output_prefix, sampling_mode=args.sampling_mode)
+    run_sampling_script(genes_without_pos_file=f"{args.output_prefix}_genes_without_pos.bed", genes_without_pos=f"{args.output_prefix}_genes_without_pos.fa", positives_file = f"{args.input_pos.split('.')[0]}.fa", positives_bed = f"{args.input_pos.split('.')[0]}.bed", input_low_conf1=args.input_low_conf1, input_low_conf2=args.input_low_conf2, num_samples=args.num_samples, output_prefix=args.output_prefix, sampling_mode=args.sampling_mode)
 
 if __name__ == "__main__":
     main()
